24c4a24f9d6fb933be6776b2ed611bcc9f6b1d25
josei <josei@DESKTOP-48TQE4N>
2021-01-29 13:22:46 +0000

    IcebergTracker V2.1

--- src/icebergtracker/IcebergTracker.java
+++ src/icebergtracker/IcebergTracker.java
@@ -2,7 +2,8 @@ package icebergtracker;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 import java.util.List;
 
 /**
@@ -11,21 +12,21 @@ import java.util.List;
  */
 public class IcebergTracker {
 
-    private final List <IcebergData> icebergsData = new ArrayList<>();
+    private final MultiValuedMap <Iceberg,IcebergData> icebergsData = new ArrayListValuedHashMap<>();
 
     public IcebergTracker() {
         
         Iceberg iceberg1 = new Iceberg(1, "cabo ABC", LocalDate.of(2017, Month.MARCH, 1));
         Iceberg iceberg2 = new Iceberg(2, "bahía DEF", LocalDate.of(2017, Month.MARCH, 10));
         
-        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg1, "88ºN 24'E", 9e9));
-        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg1, "80ºN 12'E", 8e8));
-        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg2, "77ºS 02'W", 7e7));
-        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg2, "78ºS 03'W", 6.5e7));
+        icebergsData.put(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg1, "88ºN 24'E", 9e9));
+        icebergsData.put(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg1, "80ºN 12'E", 8e8));
+        icebergsData.put(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg2, "77ºS 02'W", 7e7));
+        icebergsData.put(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg2, "78ºS 03'W", 6.5e7));
     }
     
     private void output() {
-        for (IcebergData d: icebergsData){
+        for (IcebergData d: icebergsData.values()){
             System.out.println(d);
         }
     }
