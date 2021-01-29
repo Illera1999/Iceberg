5d753b86461eb46c70c6e800e4f1bc88ac6b5d27
josei <josei@DESKTOP-48TQE4N>
2021-01-29 12:57:10 +0000

    IcebergTracker V2

---
+++ src/icebergtracker/IcebergData.java
@@ -0,0 +1,39 @@
+package icebergtracker;
+
+import java.time.LocalDate;
+
+public class IcebergData {
+
+    private final LocalDate date;
+    private final Iceberg iceberg;
+    private final String coordinates;
+    private final double volume;
+
+    public IcebergData(LocalDate date, Iceberg iceberg, String coordinates, double volume) {
+        this.date = date;
+        this.iceberg = iceberg;
+        this.coordinates = coordinates;
+        this.volume = volume;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Iceberg getIceberg() {
+        return iceberg;
+    }
+
+    public String getCoordenadas() {
+        return coordinates;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+    
+    @Override
+    public String toString(){
+        return iceberg + ", en fecha: " + date + ", volumen: " + volume + ", coordenadas: " + coordinates;
+    }
+}
--- src/icebergtracker/IcebergTracker.java
+++ src/icebergtracker/IcebergTracker.java
@@ -11,40 +11,22 @@ import java.util.List;
  */
 public class IcebergTracker {
 
-    private final List <Iceberg> iceberg = new ArrayList <>();
-    private final List <IcebergPosition> position = new ArrayList <>();
-    private final List <IcebergVolume> volume = new ArrayList <>();
+    private final List <IcebergData> icebergsData = new ArrayList<>();
 
     public IcebergTracker() {
         
+        Iceberg iceberg1 = new Iceberg(1, "cabo ABC", LocalDate.of(2017, Month.MARCH, 1));
+        Iceberg iceberg2 = new Iceberg(2, "bahía DEF", LocalDate.of(2017, Month.MARCH, 10));
         
-        iceberg.add(new Iceberg(1, "Cabo ABC", LocalDate.of(2017, Month.MARCH, 1)));
-        iceberg.add(new Iceberg(2, "Bahía DEF", LocalDate.of(2017, Month.MARCH, 10)));
-        
-        position.add(new IcebergPosition(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg.get(0), "88ºN 24'E"));
-        position.add(new IcebergPosition(LocalDate.of(2018, Month.OCTOBER, 1), iceberg.get(0), "80ºN 12'E"));
-        position.add(new IcebergPosition(LocalDate.of(2018, Month.SEPTEMBER, 1),iceberg.get(1), "77ºS 02'W"));
-        position.add(new IcebergPosition(LocalDate.of(2018, Month.OCTOBER, 1), iceberg.get(1), "78ºS 03'W"));
-        
-        volume.add(new IcebergVolume(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg.get(0), 9e9));
-        volume.add(new IcebergVolume(LocalDate.of(2018, Month.OCTOBER, 1), iceberg.get(0), 8e8));
-        volume.add(new IcebergVolume(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg.get(1), 7e7));
-        volume.add(new IcebergVolume(LocalDate.of(2018, Month.OCTOBER, 1), iceberg.get(1), 6.5e7));
+        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg1, "88ºN 24'E", 9e9));
+        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg1, "80ºN 12'E", 8e8));
+        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.SEPTEMBER, 1), iceberg2, "77ºS 02'W", 7e7));
+        icebergsData.add(new IcebergData(LocalDate.of(2018, Month.OCTOBER, 1), iceberg2, "78ºS 03'W", 6.5e7));
     }
     
     private void output() {
-        for(Iceberg i : iceberg) {
-            System.out.println(i.toString());
-            for(IcebergPosition p : position) {
-                if(p.getIceberg().equals(i)) {
-                    System.out.println(p.toString());
-                    for(IcebergVolume v : volume) {
-                        if(v.getIceberg().equals(i)
-                                && v.getDate().equals(p.getDate()))
-                            System.out.println(v);
-                    }
-                }
-            }
+        for (IcebergData d: icebergsData){
+            System.out.println(d);
         }
     }
               
